f1b986e213a2b30961dec15040f9c25d81e61da7
Birkan <birkanturan35@gmail.com>
2025-06-18 14:51:24 +0300

    DbContext sınıfı oluşturuldu ve EF Core paketleri eklendi.

--- DoneIt.DataAccessLayer/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DoneIt.DataAccessLayer
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ DoneIt.DataAccessLayer/Context.cs
@@ -0,0 +1,20 @@
+﻿using DoneIt.EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoneIt.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-G13T04V9\\SQLEXPRESS;Database=DoneItDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
